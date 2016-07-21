e0cb95c73a6e55ea0f8da07727969aa356fa0982
Itai Yavin <itai.yavin@gmail.com>
2016-07-22 00:00:23 +0200

    Made harpoon script and prefab. - Applies force at start - Destroys itself when below y height equal to variable killAltitude
    Harpoongun script
    - Velocity vector controls shooting direction
    - A and D keys rotates vector (aim functionality)
    - Shoot function (instantiates harpoon object and sets its velocity)
    - Space key for shooting

---
+++ Assets/Scripts/Harpoon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Harpoon : MonoBehaviour {
+
+	public Vector3 velocity;
+	public float killAltitude;
+	private float speed;
+	private Rigidbody rig;
+
+
+	// Use this for initialization
+	void Start () {
+		speed = 2000f;
+		killAltitude = -5f;
+		rig = gameObject.GetComponent (typeof (Rigidbody)) as Rigidbody;
+		rig.AddForce (velocity * speed);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < killAltitude)
+			Destroy (gameObject);
+	}
+}
