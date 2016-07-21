09cdf8f4d68d774406d781040b04d7df06e758c0
¨KasperHdL¨ <kasper@honnens.dk>
2016-07-21 23:19:41 +0200

    get;set isDead for the surfer

---
+++ Assets/Scripts/Surfer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Surfer : MonoBehaviour {
+
+	private bool _isDead = false;
+	public bool isDead{
+		get { return _isDead;}
+		private set {_isDead = value;}
+	}
+	
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
