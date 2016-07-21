1880adedeb3be311babafb526cd81df8b51f9d4f
Frans Peter Larsen <larsen.frans@gmail.com>
2016-07-22 00:41:52 +0200

    Some win conditions & current seconds

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class GameState {
+    public static Team[] teams;
+
+    public static float seconds;
+
+    public static void CheckIfTeamWon()
+    {
+        int index = -1;
+        int numOfAliveSurfers = 0;
+
+        for(int i = 0; i < teams.Length;i++)
+        {
+            if (!teams[i].surf.isDead)
+            {
+                numOfAliveSurfers++;
+                index = i;
+            }
+
+            if (numOfAliveSurfers > 1)
+                return;
+        }
+
+        if(index == -1 || index > teams.Length) {
+            Debug.LogError("Bad Team Index");
+        }
+
+        TeamWon(teams[index]);
+    }
+
+    private static void TeamWon(Team team)
+    {
+
+    }
+}
+
+[System.Serializable]
+public struct Team
+{
+    public String name;
+    public Color color;
+    public Boat boat;
+    public Surfer surf;
+}
---
+++ Assets/Scripts/GameStateInspector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateInspector : MonoBehaviour {
+    public Team[] teams;
+
+    private float startTime;
+
+    // Use this for initialization
+    void Start ()
+    {
+        startTime = Time.realtimeSinceStartup;
+
+        GameState.teams = this.teams;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        GameState.seconds = Time.realtimeSinceStartup - startTime;
+    }
+}
