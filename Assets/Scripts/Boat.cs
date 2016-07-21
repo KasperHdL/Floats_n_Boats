41b52a8c306e74c0cbed0c9a642a0ef098ad15f4
¨KasperHdL¨ <kasper@honnens.dk>
2016-07-21 21:22:16 +0200

    Initial Boat and folders...

---
+++ Assets/Scripts/Boat.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boat : MonoBehaviour {
+	private Rigidbody body;
+
+	[SerializeField] private float motorOffset;
+	[SerializeField] private float motorMaxAngle;
+	[SerializeField] private float thrust;
+
+
+	void Start () {
+		body = GetComponent<Rigidbody>();
+	}
+
+
+	void Update () {
+
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		if(v < 0) v = 0;
+
+		Vector3 force = transform.forward * thrust * v * Time.deltaTime;
+		force = Quaternion.Euler(0, motorMaxAngle * -h, 0) * force;
+
+		Debug.DrawLine(transform.position + transform.forward * motorOffset, transform.position + transform.forward * motorOffset + force);
+		body.AddForceAtPosition(force, transform.position + transform.forward * motorOffset);
+	}
+}
