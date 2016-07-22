ecf838be3eb65e3308e3957ff01cec3549dca299
¨KasperHdL¨ <kasper@honnens.dk>
2016-07-22 11:23:40 +0200

    Added reversing to the boat, and tweaked slightly...

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -19,12 +19,14 @@ public class Boat : MonoBehaviour {
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
 
-		if(v < 0) v = 0;
-
+		if(v < 0)
+			v = v * 0.1f;
+	
 		Vector3 force = transform.forward * thrust * v * Time.deltaTime;
 		force = Quaternion.Euler(0, motorMaxAngle * -h, 0) * force;
 
 		Debug.DrawLine(transform.position + transform.forward * motorOffset, transform.position + transform.forward * motorOffset + force);
 		body.AddForceAtPosition(force, transform.position + transform.forward * motorOffset);
+			
 	}
 }
