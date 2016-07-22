067a0f635759c5f24583e618d1a8ccf539773e7a
Frans Peter Larsen <larsen.frans@gmail.com>
2016-07-22 14:45:35 +0200

    Merged Time with GameState and Cleaned Script

--- Assets/Scripts/DayCycle.cs
+++ Assets/Scripts/DayCycle.cs
@@ -1,26 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DayCycle : MonoBehaviour {
+public class DayCycle : MonoBehaviour
+{
+    private Quaternion startRot;
+    private Quaternion endRot;
 
-	[SerializeField]
-	private float dayLength;
+    // Use this for initialization
+    void Start()
+    {
+        startRot = Quaternion.Euler(90, 0, 0);
+        endRot = Quaternion.Euler(-180, 0, 0);
+    }
 
-	private float percent;
-	private Quaternion startRot;
-	private Quaternion endRot;
-
-	// Use this for initialization
-	void Start () {
-		startRot = Quaternion.Euler (90,0,0);
-		endRot = Quaternion.Euler (-180, 0, 0);
-		dayLength = 10f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Debug.Log (percent);
-		percent = GameState.seconds / dayLength;
-		transform.rotation = Quaternion.Lerp(startRot, endRot, percent);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        float t = GameState.roundTimeSeconds / GameState.roundLengthSeconds;
+        transform.rotation = Quaternion.Lerp(startRot, endRot, t);
+    }
 }
