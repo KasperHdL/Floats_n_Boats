cafc4a4c5ad33ceff546c684ddc137fe81778077
Itai Yavin <itai.yavin@gmail.com>
2016-07-22 14:35:51 +0200

    Made a script for directional light to rotate, as to imitate a daycycle

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayCycle : MonoBehaviour {
+
+	[SerializeField]
+	private float dayLength;
+
+	private float percent;
+	private Quaternion startRot;
+	private Quaternion endRot;
+
+	// Use this for initialization
+	void Start () {
+		startRot = Quaternion.Euler (90,0,0);
+		endRot = Quaternion.Euler (-180, 0, 0);
+		dayLength = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (percent);
+		percent = GameState.seconds / dayLength;
+		transform.rotation = Quaternion.Lerp(startRot, endRot, percent);
+	}
+}
