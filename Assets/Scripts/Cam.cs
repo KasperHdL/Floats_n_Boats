4adb28e9eb074a9495b160b2d9014ce3b35456ee
¨KasperHdL¨ <kasper@honnens.dk>
2016-07-24 14:29:03 +0200

    Final Camera and Resetable game

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -8,9 +8,35 @@ public class Cam : MonoBehaviour
     public Vector3 right = Vector3.right;
     public Camera camera;
 
+    public Vector3 menuPos;
+    public Quaternion menuRotation;
+    private float menuFOV;
+
+    [SerializeField] private float maxSpeed = 0.5f;
+    [SerializeField] private float maxFovChange = 0.5f;
+
+
+    [SerializeField] private float V3UpMax = 8f;
+    [SerializeField] private float V3UpMin = 4f;
+
+    [SerializeField] private float V3OneMin = 10f;
+    [SerializeField] private float V3OneMax = 20f;
+    
+    [SerializeField] private float FOVMin = 25f;
+    [SerializeField] private float FOVMax = 30f;
+
+
     private void Start()
     {
+        
+
+        menuPos = transform.position;
+        menuRotation = transform.rotation;
+
         camera = GetComponent<Camera>();
+        menuFOV = camera.fieldOfView;
+
+        this.enabled = false;
     }
 
     private void Update()
@@ -32,16 +58,67 @@ public class Cam : MonoBehaviour
                 }
             }
             average /= players.Length;
-            transform.position = Vector3.Lerp(transform.position, average + (Vector3.one + Vector3.up) * 8, 0.2f);
-            transform.LookAt(transform.position - (Vector3.one + Vector3.up));
+            
+            float scaledDist = Mathf.Clamp((dist - 10) / 50, 0f, 1f);
+            
+            float V3One = scaledDist * V3OneMax + V3OneMin;
+            float V3Up = scaledDist * (V3UpMax - V3UpMin) + V3UpMin;
+            float FOV = scaledDist * FOVMax + FOVMin;
+            
+            Vector3 desiredPosition = average + Vector3.one * V3One + Vector3.up * V3Up;
 
-            if (players.Length > 1)
-                camera.fieldOfView = dist + 40;
-            else
+            Vector3 delta = desiredPosition - transform.position;
+            if(delta.magnitude > maxSpeed)
+                delta = delta.normalized * maxSpeed;
+
+            
+            transform.position = Vector3.Lerp(transform.position, transform.position + delta, 0.5f);
+
+
+            Quaternion desiredRotation = Quaternion.LookRotation(-(Vector3.one + Vector3.up));
+                
+            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, 0.1f);
+
+            if (players.Length > 1){
+                float desiredFOV = FOV;
+                float deltaFOV = desiredFOV - camera.fieldOfView;
+                if(Mathf.Abs(deltaFOV) > maxFovChange)
+                    deltaFOV = Mathf.Sign(deltaFOV) * maxFovChange;
+                camera.fieldOfView = camera.fieldOfView + deltaFOV;
+            }else
                 camera.orthographicSize = Mathf.Lerp(Camera.current.fieldOfView, 40, 1f);
 
             right = transform.right;
             up = transform.forward;
         }
     }
+
+    public void lerpToMenu(float animationLength){
+        StartCoroutine(routine_lerpToMenu(animationLength ));
+    }
+
+    IEnumerator routine_lerpToMenu(float length){
+        Vector3 startPosition = transform.position;
+        Quaternion startRotation = transform.rotation;
+        float startFOV = camera.fieldOfView;
+
+        float startTime = Time.time - 1f;
+        float t = 0;
+        
+        while(startTime + length > Time.time){
+            
+            t = (Time.time - startTime) / length;
+            transform.position = Vector3.Lerp(startPosition, menuPos, t);
+            transform.rotation = Quaternion.Lerp(startRotation, menuRotation, t);
+            camera.fieldOfView = Mathf.Lerp(startFOV, menuFOV, t);
+            
+            yield return null;
+        }
+        
+        transform.position = menuPos;
+        transform.rotation = menuRotation;
+        
+        yield return new WaitForSeconds(1f);
+        this.enabled = true; 
+    } 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,7 +13,11 @@ public class Player : MonoBehaviour {
 	public bool ai_controlled = false;
 	[SerializeField, Range(0,1)] private float ai_turnChance = 0.5f;
 
+	private Vector3 menuPos;
+	private Quaternion menuRotation;
     void Start() {
+		menuPos = transform.position;
+		menuRotation = transform.rotation;
 		if(!controlled) return;
         gamepad = new GamePad(joystickNumber);
         controllable = GetComponent<Controllable>();
@@ -42,5 +46,30 @@ public class Player : MonoBehaviour {
 			controllable.InputUpdate(stick, aimStick, gamepad.GetButtonDown(GamePad.Button.RB));
 		}
 		
-	}
+	}	
+    public void lerpToMenu(float animationLength, float offsetY){
+        StartCoroutine(routine_lerpToMenu(animationLength, offsetY));
+    }
+
+    IEnumerator routine_lerpToMenu(float length, float offsetY){
+        this.enabled = false;
+		
+        Vector3 startPosition = transform.position;
+        Quaternion startRotation = transform.rotation;
+
+        float startTime = Time.time;
+        float t = 0;
+        
+        while(startTime + length > Time.time){
+            
+            t = (Time.time - startTime) / length;
+            transform.position = Vector3.Lerp(startPosition, menuPos + Vector3.up * offsetY, t);
+            transform.rotation = Quaternion.Lerp(startRotation, menuRotation, t);
+            
+            yield return null;
+        }
+
+		this.enabled = true;
+    }
+
 }
