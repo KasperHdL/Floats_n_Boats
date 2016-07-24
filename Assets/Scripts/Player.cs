d3d473f92ccebcb97da4eca9530dc7d85d6c3041
¨KasperHdL¨ <kasper@honnens.dk>
2016-07-24 11:57:59 +0200

    Start Menu Startable by one person, Boat Rotation according to vel
    And Motor Rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,23 @@ public class Player : MonoBehaviour {
 
 	public int joystickNumber;
     public GamePad gamepad;
-
+	public bool controlled = true;
 	private Controllable controllable;
 	private bool isControllingBoat;
 
 	[Header("Super Simple AI")]
-	[SerializeField] private bool ai_controlled = false;
+	public bool ai_controlled = false;
 	[SerializeField, Range(0,1)] private float ai_turnChance = 0.5f;
 
     void Start() {
+		if(!controlled) return;
         gamepad = new GamePad(joystickNumber);
         controllable = GetComponent<Controllable>();
         isControllingBoat = controllable is Boat;
     }
 
 	void FixedUpdate () {
+		if(!controlled)return;
 
 		Vector2 stick;
 		
